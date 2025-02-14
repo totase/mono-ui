816670acc5f00ff8109b3fa75849ac4506ed2dca
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-02-14 18:00:16 +0100

    chore: Adjust config + format

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as Element).render(
   <StrictMode>
     <App />
   </StrictMode>,
