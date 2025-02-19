e3d83a6d7ef625a71a1e047f83c9161fe3a7b1de
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-02-19 14:51:17 +0100

    feat(radio): Add radio component

---
+++ src/components/Radio/index.tsx
@@ -0,0 +1,46 @@
+import { InputHTMLAttributes } from 'react';
+import cx from 'clsx';
+
+import { Typography } from '..';
+
+import './styles.css';
+
+export interface RadioProps extends InputHTMLAttributes<HTMLInputElement> {
+  id: string;
+  label: string;
+  description?: string;
+}
+
+const Radio = ({ id, label, disabled, description, readOnly, ...rest }: RadioProps) => {
+  const classNames = cx('mono-ui__radio', {
+    'mono-ui__radio--disabled': disabled,
+    'mono-ui__radio--readOnly': readOnly,
+  });
+
+  return (
+    <div className="mono-ui__radio__container">
+      <span className={classNames}>
+        <input
+          id={id}
+          type="radio"
+          disabled={disabled}
+          readOnly={readOnly}
+          aria-describedby={description ? `${id}-description` : undefined}
+          {...rest}
+        />
+      </span>
+
+      <label className="mono-ui__radio__label" htmlFor={id}>
+        {label}
+
+        {description && (
+          <Typography id={`${id}-description`} as="span">
+            {description}
+          </Typography>
+        )}
+      </label>
+    </div>
+  );
+};
+
+export default Radio;
