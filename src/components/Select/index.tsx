4e64194c064b78332cdedbfcc41dc8a73905dbdf
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-02-22 19:20:09 +0100

    feat(select): Add select component

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,41 @@
+import { InputHTMLAttributes } from 'react';
+import cx from 'clsx';
+
+import './styles.css';
+
+export type SelectOptionType = {
+  label: string;
+  value: string | number;
+};
+
+export interface SelectProps extends Omit<InputHTMLAttributes<HTMLSelectElement>, 'children'> {
+  id: string;
+  label: string;
+  options: SelectOptionType[];
+}
+
+const Select = ({ id, label, disabled, options, placeholder = 'Select a value', ...rest }: SelectProps) => {
+  const classNames = cx('mono-ui__select', {
+    'mono-ui__select--disabled': disabled,
+  });
+
+  return (
+    <div className="mono-ui__select__container">
+      <label htmlFor={id}>{label}</label>
+
+      <select id={id} disabled={disabled} className={classNames} {...rest}>
+        <option value={undefined} className="mono-ui__select__placeholder">
+          {placeholder}
+        </option>
+
+        {options.map(({ value, label }, i) => (
+          <option key={`${value}-${i}`} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export { default as Checkbox } from './Checkbox';
 export { default as Input } from './Input';
 export { default as Label } from './Label';
 export { default as Radio } from './Radio';
+export { default as Select } from './Select';
 export { default as Typography } from './Typography';
