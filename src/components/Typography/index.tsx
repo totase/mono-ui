43972bba8afcdde8de9aed230d6f20e0ae7f57f2
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-02-23 19:52:00 +0100

    fix(typography): Adjust typography props and styling

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -6,12 +6,26 @@ import './styles.css';
 export interface TypographyProps<T extends ElementType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span'>
   extends HTMLAttributes<HTMLElement> {
   light?: boolean;
+  weight?: 'regular' | 'medium' | 'bold';
+  tiny?: boolean;
   as?: T;
 }
 
-const Typography = ({ children, className, light = false, as: Component = 'p', ...rest }: TypographyProps) => {
+const Typography = ({
+  children,
+  className,
+  light = false,
+  tiny = false,
+  as: Component = 'p',
+  weight = Component === 'p' || Component === 'span' ? 'regular' : 'bold',
+  ...rest
+}: TypographyProps) => {
   const classNames = cx('mono-ui__typography', className, {
     'mono-ui__typography--light': light,
+    'mono-ui__typography--regular': weight === 'regular',
+    'mono-ui__typography--medium': weight === 'medium',
+    'mono-ui__typography--bold': weight === 'bold',
+    'mono-ui__typography--tiny': tiny,
   });
 
   return (
