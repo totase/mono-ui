3527d1d7baca7afe65d80275b22315b4886231c8
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-02-25 11:57:16 +0100

    fix(badge): Add appearance prop + fix styling

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,18 +1,21 @@
 import { HTMLAttributes } from 'react';
 import cx from 'clsx';
 
+import { Appearance } from '@/types';
+
 import './styles.css';
 
 export interface BadgeProps extends HTMLAttributes<HTMLSpanElement> {
   children: string;
   tiny?: boolean;
-  outline?: boolean;
+  appearance?: Appearance;
 }
 
-const Badge = ({ children, tiny = false, outline = false, ...rest }: BadgeProps) => {
+const Badge = ({ children, appearance = 'default', tiny = false, ...rest }: BadgeProps) => {
   const classNames = cx('mono-ui__badge', {
-    'mono-ui__badge--default': !outline,
-    'mono-ui__badge--outline': outline,
+    'mono-ui__badge--default': appearance === 'default',
+    'mono-ui__badge--outline': appearance === 'outline',
+    'mono-ui__badge--light': appearance === 'light',
     'mono-ui__badge--tiny': tiny,
   });
 
