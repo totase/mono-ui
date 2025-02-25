c3b1d1afaf9c84dd70eaa6baff069dcc5d19c8ad
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-02-25 11:40:57 +0100

    feat(link): Add link component

---
+++ src/components/Link/index.tsx
@@ -0,0 +1,20 @@
+import { AnchorHTMLAttributes, ReactNode } from 'react';
+import cx from 'clsx';
+
+import './styles.css';
+
+export interface LinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  children: ReactNode;
+}
+
+const Link = ({ href, children, className, ...rest }: LinkProps) => {
+  const classNames = cx('mono-ui__link', className);
+
+  return (
+    <a href={href} className={classNames} {...rest}>
+      {children}
+    </a>
+  );
+};
+
+export default Link;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
 export { default as Input } from './Input';
 export { default as Label } from './Label';
+export { default as Link } from './Link';
 export { default as Radio } from './Radio';
 export { default as Select } from './Select';
 export { default as Toggle } from './Toggle';
