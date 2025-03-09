31eacf5575599cc018fe151eac775e2c09fac544
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-03-09 20:15:54 +0100

    feat(flex): Add flex component

---
+++ src/components/Flex/index.tsx
@@ -0,0 +1,39 @@
+import { HTMLAttributes } from 'react';
+import cx from 'clsx';
+
+import './styles.css';
+
+export interface FlexProps extends HTMLAttributes<HTMLDivElement> {
+  justify?: 'center' | 'start' | 'end' | 'space-between' | 'space-around' | 'space-evenly';
+  align?: 'center' | 'start' | 'end' | 'baseline' | 'stretch';
+  gap?: 'sm' | 'md' | 'lg';
+  direction?: 'row' | 'column' | 'row-reverse' | 'column-reverse';
+  wrap?: boolean;
+}
+
+const Flex = ({
+  children,
+  className,
+  justify,
+  align,
+  gap = 'md',
+  direction = 'row',
+  wrap = true,
+  ...rest
+}: FlexProps) => {
+  const classNames = cx('mono-ui-flex', className, {
+    [`mono-ui-flex--justify-${justify}`]: !!justify,
+    [`mono-ui-flex--align-${align}`]: !!align,
+    [`mono-ui-flex--gap-${gap}`]: gap,
+    [`mono-ui-flex--direction-${direction}`]: direction,
+    'mono-ui-flex--wrap': wrap,
+  });
+
+  return (
+    <div className={classNames} {...rest}>
+      {children}
+    </div>
+  );
+};
+
+export default Flex;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Badge } from './Badge';
 export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
+export { default as Flex } from './Flex';
 export { default as Horizontal } from './Horizontal';
 export { default as Input } from './Input';
 export { default as Label } from './Label';
