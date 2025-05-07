dbee3729101157950981a962fa1537b343137834
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-05-07 14:31:11 +0200

    chore: Add test setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 import react from '@vitejs/plugin-react';
 
@@ -19,5 +19,11 @@ export default defineConfig({
         },
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+    pool: 'forks',
   },
 });
