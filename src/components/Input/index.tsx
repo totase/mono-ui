e055d748c1cdf5b1cfde830eb7b2c678acd8363c
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-05-12 12:36:59 +0200

    fix(input): Add label component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,15 +1,18 @@
 import { InputHTMLAttributes } from 'react';
 import cx from 'clsx';
 
+import { Label } from '../Typography';
+
 import './styles.css';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   id: string;
   label: string;
+  description?: string;
   type?: 'text' | 'password' | 'email' | 'number';
 }
 
-const Input = ({ id, label, className, type = 'text', disabled, readOnly, ...rest }: InputProps) => {
+const Input = ({ id, label, description, className, type = 'text', disabled, readOnly, ...rest }: InputProps) => {
   const classNames = cx('mono-ui-input', className, {
     'mono-ui-input--disabled': disabled,
     'mono-ui-input--readOnly': readOnly,
@@ -17,8 +20,18 @@ const Input = ({ id, label, className, type = 'text', disabled, readOnly, ...res
 
   return (
     <div className="mono-ui-input__container">
-      <label htmlFor={id}>{label}</label>
-      <input id={id} type={type} className={classNames} disabled={disabled} readOnly={readOnly} {...rest} />
+      <Label id={id} description={description}>
+        {label}
+      </Label>
+      <input
+        id={id}
+        type={type}
+        className={classNames}
+        disabled={disabled}
+        readOnly={readOnly}
+        aria-describedby={description ? `${id}-description` : undefined}
+        {...rest}
+      />
     </div>
   );
 };
