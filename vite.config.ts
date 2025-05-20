b19f7283c1b85d711495ba97c461b9a45c158804
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-05-20 11:40:50 +0200

    chore: Fix scripts + build options

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: './src/index.ts',
-      name: 'Mono UI',
-      fileName: 'mono-ui',
-    },
-    rollupOptions: {
-      external: ['react'],
-      output: {
-        globals: {
-          react: 'React',
-        },
-      },
-    },
-  },
   test: {
     globals: true,
     environment: 'jsdom',
