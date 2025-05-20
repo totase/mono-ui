1aeb3cb80889d408458983815f0f92c05840d303
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-05-20 09:06:25 +0200

    chore: Export components and types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-import './styles/global.css';
+import './styles/index.css';
 
-export type { Appearance, Position, Size, Weight } from './types';
+export type { Appearance, Position, Size, Color, Weight, Align } from './types';
+
+export { Accordion, type AccordionProps } from './components';
+export { Arrow, type ArrowProps } from './components';
+export { Badge, type BadgeProps } from './components';
+export { Button, type ButtonProps } from './components';
+export { Card, type CardProps } from './components';
+export { Checkbox, type CheckboxProps } from './components';
+export { Flex, type FlexProps } from './components';
+export { Horizontal, type HorizontalProps } from './components';
+export { Input, type InputProps } from './components';
+export { Link, type LinkProps } from './components';
+export { Modal, type ModalProps } from './components';
+export { Pagination, type PaginationProps } from './components';
+export { Popover, type PopoverProps } from './components';
+export { Radio, type RadioProps } from './components';
+export { Select, type SelectProps } from './components';
+export { Slot, type SlotProps } from './components';
+export { Table, type TableProps } from './components';
+export { Tabs, type TabsProps } from './components';
+export { Toggle, type ToggleProps } from './components';
+export { Tooltip, type TooltipProps } from './components';
+export { Body, type BodyProps } from './components/Typography';
+export { Heading, type HeadingProps } from './components/Typography';
+export { Label, type LabelProps } from './components/Typography';
