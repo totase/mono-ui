88424e942bf6c3d54433684b4153046a73b8e398
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-07-03 09:32:09 +0200

    chore(storybook): Add a11y config

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,8 +1,10 @@
 import { beforeAll } from 'vitest';
 import { setProjectAnnotations } from '@storybook/react-vite';
+// eslint-disable-next-line import/namespace
+import * as a11yAddonAnnotations from '@storybook/addon-a11y/preview';
 
 import * as previewAnnotations from './preview';
 
-const annotations = setProjectAnnotations([previewAnnotations]);
+const annotations = setProjectAnnotations([a11yAddonAnnotations, previewAnnotations]);
 
 beforeAll(annotations.beforeAll);
