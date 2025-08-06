2165155bc5f6d6f63f3b09e9eded58b05b244797
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-08-06 07:50:26 +0200

    fix(combobox): Use Chips as selected options

--- src/components/Combobox/components/ComboboxSelectedOptions.tsx
+++ src/components/Combobox/components/ComboboxSelectedOptions.tsx
@@ -1,27 +1,25 @@
 import { useContext } from 'react';
 
+import Chips from '../../Chips';
 import { ComboboxOptionsContext, ComboboxOptionsContextProps } from '../context';
 
 const ComboboxSelectedOptions = () => {
   const { selectedOptions, removeSelectedOption } = useContext(ComboboxOptionsContext) as ComboboxOptionsContextProps;
 
   return (
-    <ul className="mono-ui-combobox__selected-options">
+    <Chips className="mono-ui-combobox__selected-options">
       {selectedOptions.map((option) => (
-        <li key={option.value}>
-          <button
-            className="mono-ui-combobox__selected-option"
-            aria-label={`Remove ${option.value}`}
-            onClick={(e) => {
-              e.stopPropagation();
-              removeSelectedOption(option);
-            }}
-          >
-            {option.label} &#10005;
-          </button>
-        </li>
+        <Chips.Removable
+          key={option.value}
+          onClick={(e) => {
+            e.stopPropagation();
+            removeSelectedOption(option);
+          }}
+        >
+          {option.label}
+        </Chips.Removable>
       ))}
-    </ul>
+    </Chips>
   );
 };
 
