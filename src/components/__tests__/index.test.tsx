0579a8bb086a46adefa3029a36933f95fc7337d6
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-10-06 12:15:48 +0200

    fix: Add export test for components

---
+++ src/components/__tests__/index.test.tsx
@@ -0,0 +1,144 @@
+import {
+  Accordion,
+  Arrow,
+  Badge,
+  Button,
+  Card,
+  Checkbox,
+  Chips,
+  Combobox,
+  Flex,
+  Grid,
+  Horizontal,
+  Input,
+  Link,
+  List,
+  Modal,
+  Pagination,
+  Popover,
+  Radio,
+  Select,
+  Slot,
+  Table,
+  Tabs,
+  Textarea,
+  Toggle,
+  Tooltip,
+  Body,
+  Heading,
+  Label,
+} from '..';
+
+describe('index', () => {
+  test('should export Accordion component', () => {
+    expect(Accordion).toBeInstanceOf(Function);
+  });
+
+  test('should export Arrow component', () => {
+    expect(Arrow).toBeInstanceOf(Function);
+  });
+
+  test('should export Badge component', () => {
+    expect(Badge).toBeInstanceOf(Function);
+  });
+
+  test('should export Button component', () => {
+    expect(Button).toBeInstanceOf(Function);
+  });
+
+  test('should export Card component', () => {
+    expect(Card).toBeInstanceOf(Function);
+  });
+
+  test('should export Checkbox component', () => {
+    expect(Checkbox).toBeInstanceOf(Function);
+  });
+
+  test('should export Chips component', () => {
+    expect(Chips).toBeInstanceOf(Function);
+  });
+
+  test('should export Combobox component', () => {
+    expect(Combobox).toBeInstanceOf(Function);
+  });
+
+  test('should export Flex component', () => {
+    expect(Flex).toBeInstanceOf(Function);
+  });
+
+  test('should export Grid component', () => {
+    expect(Grid).toBeInstanceOf(Function);
+  });
+
+  test('should export Horizontal component', () => {
+    expect(Horizontal).toBeInstanceOf(Function);
+  });
+
+  test('should export Input component', () => {
+    expect(Input).toBeInstanceOf(Function);
+  });
+
+  test('should export Link component', () => {
+    expect(Link).toBeInstanceOf(Function);
+  });
+
+  test('should export List component', () => {
+    expect(List).toBeInstanceOf(Function);
+  });
+
+  test('should export Modal component', () => {
+    expect(Modal).toBeInstanceOf(Function);
+  });
+
+  test('should export Pagination component', () => {
+    expect(Pagination).toBeInstanceOf(Function);
+  });
+
+  test('should export Popover component', () => {
+    expect(Popover).toBeInstanceOf(Function);
+  });
+
+  test('should export Radio component', () => {
+    expect(Radio).toBeInstanceOf(Function);
+  });
+
+  test('should export Select component', () => {
+    expect(Select).toBeInstanceOf(Function);
+  });
+
+  test('should export Slot component', () => {
+    expect(Slot).toBeInstanceOf(Function);
+  });
+
+  test('should export Table component', () => {
+    expect(Table).toBeInstanceOf(Function);
+  });
+
+  test('should export Tabs component', () => {
+    expect(Tabs).toBeInstanceOf(Function);
+  });
+
+  test('should export Textarea component', () => {
+    expect(Textarea).toBeInstanceOf(Function);
+  });
+
+  test('should export Toggle component', () => {
+    expect(Toggle).toBeInstanceOf(Function);
+  });
+
+  test('should export Tooltip component', () => {
+    expect(Tooltip).toBeInstanceOf(Function);
+  });
+
+  test('should export Body component', () => {
+    expect(Body).toBeInstanceOf(Function);
+  });
+
+  test('should export Heading component', () => {
+    expect(Heading).toBeInstanceOf(Function);
+  });
+
+  test('should export Label component', () => {
+    expect(Label).toBeInstanceOf(Function);
+  });
+});
